718c7b0e8dd1aabea23eef5165e730bacdb3f399
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-01-27 12:34:37 +0000

    Snake now moves

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,62 @@ pub mod snake_game{
 
     pub fn get_cell_size() -> f64 {WINDOW_SIZE/CELL_COUNT}
 
+
+    pub enum Direction{
+        Left,
+        Right,
+        Top,
+        Down,
+    }
+
+    impl Direction {
+        pub fn value(&self) -> [f64;2]{
+            match self {
+                Direction::Top => [0.0, -1.0],
+                Direction::Down => [0.0, 1.0],
+                Direction::Left => [-1.0, 0.0],
+                Direction::Right => [1.0, 0.0],
+            }
+        }
+    }
+
+
     pub struct Snake{
         pub scale: f64,
-        pub x: f64,
-        pub y: f64,
+        pub position: [f64;2],
         pub body: Vec<[f64;2]>,
-        pub color: [f32;4]
+        pub color: [f32;4],
+        pub direction: Direction
     }
 
     impl Snake {
         pub fn new() -> Snake{
             Snake {
                 scale: get_cell_size(),
-                x: 0.0,
-                y: 0.0,
+                position: [0.0,0.0],
                 body: vec![[0.0,0.0]],
-                color: [0.297, 1.0, 0.489, 1.0]
+                color: [0.297, 1.0, 0.489, 1.0],
+                direction: Direction::Right
             }
         }
 
         pub fn draw(&self, c: piston_window::Context, g: &mut piston_window::G2d,){
-            piston_window::rectangle(self.color, [self.x, self.y,self.scale, self.scale], c.transform, g)
+            piston_window::rectangle(self.color, [self.position[0], self.position[1],self.scale, self.scale], c.transform, g)
+        }
+
+        pub fn change_direction(&mut self, key: piston::Key){
+            match key {
+                piston::Key::A => self.direction = Direction::Left,
+                piston::Key::D => self.direction = Direction::Right,
+                piston::Key::W => self.direction = Direction::Top,
+                piston::Key::S => self.direction = Direction::Down,
+                _=>{}
+            }
         }
 
         pub fn update(&mut self){
-
+            self.position = [ (self.direction.value()[0]*get_cell_size()) + self.position[0], (self.direction.value()[1]*get_cell_size()) + self.position[1]];
+            //self.position = self.direction.value() * get_cell_size();
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use piston::{EventLoop, PressEvent, UpdateEvent};
 use snake_game_piston::snake_game;
 
 
@@ -6,19 +7,42 @@ fn main() {
         new("Snake Game", (snake_game::WINDOW_SIZE,snake_game::WINDOW_SIZE))
         .resizable(false)
         .exit_on_esc(true)
-        .vsync(true)
         .build()
-
         .expect("Error building game window!");
+    
+    let frame_rate = 3.0;
+    
+
+    window.set_max_fps(frame_rate as u64);
+
     let mut snake = snake_game::Snake::new();
 
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |c, g, _gf|{
+    let mut last_update = std::time::Instant::now();
+
+    while let Some(event) = &window.next() {
+
+        if let Some(_) = event.update_args() {
+            let elapsed = last_update.elapsed();
+            if elapsed >= std::time::Duration::from_secs_f64(1.0 / frame_rate) {
+                last_update = std::time::Instant::now();
+
+                snake.update();
+            }
+        }
+
+
+        window.draw_2d(event, |c, g, _gf|{
             piston_window::clear([0.0,0.0,0.0,1.0], g);
 
             snake.draw(c, g);
         });
+
+        event.press(|f|{
+            if let piston::Button::Keyboard(key) = f {
+                snake.change_direction(key);
+            }
+        });
+
     }
 
-    snake.update();
 }
